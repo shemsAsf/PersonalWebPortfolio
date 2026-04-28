45afdd87dfbc60fc8ea3d4631f9150d00e8109a7
shemsAsf <137190111+shemsAsf@users.noreply.github.com>
2026-04-28 11:32:33 +0200

    Added azure certificate

--- my-cv-website/src/components/Resume.tsx
+++ my-cv-website/src/components/Resume.tsx
@@ -7,6 +7,36 @@ import ResumeCapsule from "./Ui/ResumeCapsule";
 export default function Resume() {
     const { t } = useTranslation();
 
+    const experiences = [
+        {
+            key: "ivalua",
+            img: "/Img/Ivalua.png",
+        },
+        {
+            key: "harmonessens",
+            img: "/Img/harmonessens.png",
+        },
+        {
+            key: "degriff",
+            img: "/Img/degriff.png",
+        },
+    ];
+
+    const education = [
+        {
+            key: "azure",
+            img: "/Img/azure-fondamntals.png",
+        },
+        {
+            key: "epsi",
+            img: "/Img/Epsi.png",
+        },
+        {
+            key: "cmi",
+            img: "/Img/CMI.png",
+        },
+    ];
+
     return (
         <div className="w-full" id="Resume">
             <div className="w-full mb-12">
@@ -14,50 +44,36 @@ export default function Resume() {
                 <Line width="sm:w-1/3 w-full" />
             </div>
 
-            <div className="w-full grid sm:grid-cols-2 gap-10">
+            <div className="w-full grid md:grid-cols-2 gap-10">
                 <div className="flex flex-col gap-10">
                     <h3 className="text-xl sm:text-2xl font-bold sm:mb-3 -mb-3 sm:text-center lg:text-left text-left">
                         {t("professional_experience")}
                     </h3>
-                    <ResumeCapsule
-                        title={t("ivalua.title")}
-                        subtitle={t("ivalua.subtitle")}
-                        text={t("ivalua.text")}
-                        img="/Img/Ivalua.png"
-                    />
 
-                    <ResumeCapsule
-                        title={t("harmonessens.title")}
-                        subtitle={t("harmonessens.subtitle")}
-                        text={t("harmonessens.text")}
-                        img="/Img/harmonessens.png"
-                    />
-
-                    <ResumeCapsule
-                        title={t("degriff.title")}
-                        subtitle={t("degriff.subtitle")}
-                        text={t("degriff.text")}
-                        img="/Img/degriff.png"
-                    />
+                    {experiences.map((item) => (
+                        <ResumeCapsule
+                            key={item.key}
+                            title={t(`${item.key}.title`)}
+                            subtitle={t(`${item.key}.subtitle`)}
+                            text={t(`${item.key}.text`)}
+                            img={item.img}
+                        />
+                    ))}
                 </div>
 
                 <div className="flex flex-col gap-10">
                     <h3 className="text-xl sm:text-2xl font-bold sm:mb-3 -mb-3 sm:text-center lg:text-left text-left">
                         {t("education")}
                     </h3>
-                    <ResumeCapsule
-                        title={t("epsi.title")}
-                        subtitle={t("epsi.subtitle")}
-                        text={t("epsi.text")}
-                        img="/Img/Epsi.png"
-                    />
-
-                    <ResumeCapsule
-                        title={t("cmi.title")}
-                        subtitle={t("cmi.subtitle")}
-                        text={t("cmi.text")}
-                        img="/Img/CMI.png"
-                    />
+                    {education.map((item) => (
+                        <ResumeCapsule
+                            key={item.key}
+                            title={t(`${item.key}.title`)}
+                            subtitle={t(`${item.key}.subtitle`)}
+                            text={t(`${item.key}.text`)}
+                            img={item.img}
+                        />
+                    ))}
                 </div>
             </div>
         </div>
